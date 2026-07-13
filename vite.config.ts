59420c1812c478c1022e7ea5c329b62621a347f6
Emmanuel Fagite Olamide <fagiteemmanuel4@gmail.com>
2026-07-13 01:01:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin";
+import { TanStackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    TanStackRouter(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
